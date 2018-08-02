59d2ca35d58355525c50e31d570eb86050fa3705
zxc111 <zxc9007@gmail.com>
2018-08-03 01:22:27 +0800

    tmp

--- server/funds/models.py
+++ server/funds/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Funds(models.Model):
+    class Meta:
+        db_table = "funds"
+    name = models.CharField(max_length=256)
+    fundcode = models.CharField(max_length=32)
--- server/funds/views.py
+++ server/funds/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Funds
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    obj = Funds.objects.first()
+    return HttpResponse("%s: %s" %(obj.name, obj.fundcode))
